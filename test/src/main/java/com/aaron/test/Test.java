fa2df693647e3d3c058dbe5ee705cce42229ee24
Aaron <aaronzzxup@gmail.com>
2019-12-19 15:32:01 +0800

    init

--- test/src/main/java/com/aaron/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.aaron.test;
-
-public final class Test {
-
-    public static void main(String[] args) {
-
-    }
-}
--- test/src/main/java/com/aaron/test/factory/Ball.java
+++
@@ -1,9 +0,0 @@
-package com.aaron.test.factory;
-
-/**
- * @author Aaron aaronzzxup@gmail.com
- */
-public interface Ball {
-
-    void play();
-}
